9755ab96186239da30cdf8e7cdada83c777af54a
Paula Micheli <paulagomes@ciandt.com>
2026-02-10 09:05:28 -0300

    feat(application): Criando casos de uso e implementando-os.

---
+++ biblioteca/src/main/kotlin/com/example/biblioteca/application/port/service/RegisterBookService.kt
@@ -0,0 +1,36 @@
+package com.example.biblioteca.application.port.service
+
+import com.example.biblioteca.application.port.`in`.RegisterBookCommand
+import com.example.biblioteca.application.port.`in`.RegisterBookUseCase
+import com.example.biblioteca.application.port.out.BookRepositoryPort
+import com.example.biblioteca.domain.entities.Book
+import com.example.biblioteca.domain.exceptions.IsbnAlreadyExistsException
+import com.example.biblioteca.domain.vo.Author
+import com.example.biblioteca.domain.vo.Isbn
+import com.example.biblioteca.domain.vo.Title
+import org.springframework.stereotype.Service
+
+@Service // Indica ao Spring que esta classe contém a lógica de negócio (Application Service)
+class RegisterBookService(private val repository: BookRepositoryPort) : RegisterBookUseCase {
+
+    override fun execute(command: RegisterBookCommand): String {
+
+        // 1. Regra da Aplicação (Fluxo)
+        if(repository.existByIsbn(command.isbn)){
+            throw IsbnAlreadyExistsException(command.isbn)
+        }
+
+        // 2. Montagem da Entidade (Conversão String -> VO)
+        // Aqui fica explícito que estamos transformando dados "crus" em "ricos"
+        val newBook = Book(
+            title = Title(command.titulo),
+            isbn = Isbn(command.isbn),
+            author = Author(command.author),
+        )
+
+        //3. Persistência
+        repository.save(newBook)
+
+        return newBook.id.toString()
+    }
+}
